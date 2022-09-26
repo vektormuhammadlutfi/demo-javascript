3dfeec335bbae774d1b6462d735eabf3015f8dc9
Muhammad lutfi <sainteclutfi@gmail.com>
2022-09-27 00:29:42 +0800

    modify UI and use try catch

---
+++ logic.js
@@ -0,0 +1,26 @@
+document.getElementById("check").onclick = function () {
+  let getInput = document.getElementById("getInput").value;
+
+  try {
+    isPalindrome(getInput);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+function isPalindrome(string) {
+  const sanitized = string.toLowerCase().match(/[a-z0-9]/g, "");
+  return sanitized.join("") === sanitized.reverse().join("")
+    ? alert("true")
+    : alert("false");
+}
+
+// spread operator and reverse() will reverse your string every whitespace, so you dont need loop the string inside array of reversed string just join it instead.
+
+// console.log(
+//   isPalindrome("Sator Arepo Tenet Opera Rotas."), // true
+//   isPalindrome("Aku suka rajawali, bapak. Apabila wajar, aku suka."), // true
+//   isPalindrome("Kasur ini rusak."), // true
+//   isPalindrome("Tasupi dan Tika sama-sama sakit nadi pusat."), // true
+//   isPalindrome("Pada hari minggu, apakah aku turut ayah ke kota?") // false
+// );
