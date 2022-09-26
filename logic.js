3135ec4968c5a7f95fece43d5021eaf690d01e1b
Muhammad lutfi <sainteclutfi@gmail.com>
2022-09-27 06:11:11 +0800

    Update logic.js
    Improve for loop

--- logic.js
+++ logic.js
@@ -18,17 +18,27 @@ function isPalindromeBuiltIn(string) {
 // spread operator and reverse() will reverse your string every whitespace, so you dont need loop the string inside array of reversed string just join it instead.
 
 function isPalindrome(string) {
-  // find the length of a string
-  const lenghtOfString = string.length;
+  const sanitized = string
+    .toLowerCase()
+    .match(/[a-z0-9]/g, "")
+    .join("");
 
-  // loop
-  for (let reverseChar = 0; reverseChar < lenghtOfString / 2; reverseChar++) {
-    // check if first and last string are same
-    if (string[reverseChar] !== string[lenghtOfString - 1 - reverseChar]) {
-      return alert("It is not a palindrome");
+  for (
+    var reverseChar = 0;
+    reverseChar < Math.floor(sanitized.length / 2);
+    reverseChar++
+  ) {
+    //only need to run for half the string length
+    if (
+      sanitized.charAt(reverseChar) !==
+      sanitized.charAt(sanitized.length - reverseChar - 1)
+    ) {
+      // uses !== to compare characters one-by-one from the beginning and end
+      return alert("It is not a palindrome.");
     }
   }
-  return alert("It is a palindrome");
+
+  return alert("It is a palindrome.");
 }
 
 // console.log(
