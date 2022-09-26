fef4480fb0dd4932c5f30293834b93cdb60855f9
Muhammad lutfi <sainteclutfi@gmail.com>
2022-09-27 05:45:14 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,20 +2,34 @@ document.getElementById("check").onclick = function () {
   let getInput = document.getElementById("getInput").value;
 
   try {
+    isPalindromeBuiltIn(getInput);
     isPalindrome(getInput);
   } catch (err) {
     console.error(err.message);
   }
 };
 
-function isPalindrome(string) {
+function isPalindromeBuiltIn(string) {
   const sanitized = string.toLowerCase().match(/[a-z0-9]/g, "");
   return sanitized.join("") === sanitized.reverse().join("")
-    ? alert("true")
-    : alert("false");
+    ? alert("It is a palindrome. (Built In Function)")
+    : alert("It is not a palindrome. (Built In Function)");
 }
-
 // spread operator and reverse() will reverse your string every whitespace, so you dont need loop the string inside array of reversed string just join it instead.
+
+function isPalindrome(string) {
+  // find the length of a string
+  const lenghtOfString = string.length;
+
+  // loop
+  for (let reverseChar = 0; reverseChar < lenghtOfString / 2; reverseChar++) {
+    // check if first and last string are same
+    if (string[reverseChar] !== string[lenghtOfString - 1 - reverseChar]) {
+      return alert("It is not a palindrome");
+    }
+  }
+  return alert("It is a palindrome");
+}
 
 // console.log(
 //   isPalindrome("Sator Arepo Tenet Opera Rotas."), // true
